b8e2db1e83498e5a20eae15106abb52dc5f6a3bc
Pavan <harshavardhan.pinnapureddy@gmail.com>
2022-06-16 11:41:39 +0530

    First commit

--- client.py
+++ client.py
@@ -1,22 +1,58 @@
-#importing the modules
+#import the libraries
 import socket
 import threading
 
 
-HOST = "127.0.0.1"
-PORT = 1234
+HOST='127.0.0.1'
+PORT=1234
+
+#this function listens message continously from the server
+def listen_to_server(client):
+    while 1:
+        message=client.recv(2048).decode('utf-8')
+        print("New message recieved")
+        if message != '':
+            username = message.split('~')[0]
+            content = message.split('~')[1]
+            print(f"[{username}] {content}")
+        else:
+            print('Message cant be empty')
+
+#this function is to send messages from client to the server
+def send_message_to_server(client):
+    while 1:
+        message = input('Message : ')
+        if message != '':
+            client.sendall(message.encode())
+        else:
+            print('Empty Message')
+            exit(0)
+
+def communicate_to_server(client):
+    username=input('Enter username : ')
+    if username!='':
+        client.sendall(username.encode())
+    else:
+        print('Username cannot be empty')
+        exit(0)
+
+    threading.Thread(target=listen_to_server, args=(client, )).start()
+    send_message_to_server(client)
+
+
+
 def main():
-    pass
-    #creating a socket for the client side
+    #creating a socket object
     client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
     #connect to the server
     try:
         client.connect((HOST,PORT))
-        print("Successfully connected to the server")
+        print("Successfully connected to server")
     except:
-        print(f"Unable to connect to host {HOST} and port {PORT}")
+        print(f"Unable to connect to server {HOST} {PORT}")
 
+    communicate_to_server(client)
 
 if __name__=="__main__":
     main()
--- server.py
+++ server.py
@@ -1,32 +1,71 @@
-#importing the modules
+#import the libraries
 import socket
 import threading
 
+HOST='127.0.0.1'
+PORT=1234
+active_clients=[] #this contains the list of clients who are active.
 
-HOST = '127.0.0.1'
-PORT = 1234 #port can be any number between 0-65535
+#thread function that listens to the client till the server is connected
+def listen_for_messages(client, username):
+    #print(f"{username} thread")
+    #print("here")
+    while 1:
+        message=client.recv(2048).decode('utf-8')
+        #print(message)
+        if message != '':
+            final_message=username+'~'+message
+            send_messages_to_all(final_message)
+        else:
+            print(f'Message sent from client {username} is empty')
+
+#function to send message to only one client
+def send_message_to_client(client, message):
+    client.sendall(message.encode())
+
+#function to send any new message to all the users
+def send_messages_to_all(message):
+    for user in active_clients:
+        send_message_to_client(user[1], message)
+
+def print_group_participants():
+    for user in active_clients:
+        print(user[0])
+
+#function to handle client
+def client_handler(client):
+    #server will listen for client message that contains the username.
+    while 1:
+        username = client.recv(2048).decode('utf-8')
+        if username!='':
+            print(f"{username} dived in")
+            active_clients.append((username, client))
+            break
+        else:
+            print('Client username is empty')
+    threading.Thread(target=listen_for_messages, args=(client, username)).start()
 
 def main():
-    #AF_INET means we are using IPv4 Addresses
-    #SOCK_STREAM means TCP type of communication is carried
+    #AF_INET means IPV4
+    #SOCK_STREAM means TCP
     server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    #creating a atry catch block
+
     try:
-        server.bind((HOST, PORT))
-        print(f"Running the server on host {HOST} and port {PORT}")
+        server.bind((HOST,PORT))
+        print(f"Running the server on {HOST} {PORT}")
     except:
-        print(f"unable to bind to host {HOST} and port {PORT}")  #checks if the HOST and PORT addresses are connected(binded) or not.
-    #server.listen(5)
+        print(f"Unable to bind to {HOST} and port {PORT}")
 
+    #set server limt
+    server.listen(5)
 
-    #this while loop will keep listening to the client connections
-
+    #this loop is to keep listening to the client.
     while 1:
         client, address = server.accept()
-        print(f"succesfully connected to {address[0]},{address[1]}")
-    
+        print(f"Successfully connected to client {address[0]} {address[1]}")
+        threading.Thread(target=client_handler, args=(client, )).start()
 
-     
-    
-if __name__=="__main__":
+
+
+if __name__ == '__main__':
     main()
